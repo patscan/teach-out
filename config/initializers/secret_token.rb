d9e055e5ca85245ea5f63036ccbbf7c34c3f309a
Apprentice <apprentice@devbootcamp.com>
2013-05-23 14:29:39 -0700

    first commit, barebones rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TeachOut::Application.config.secret_token = '4fc132fa6352bd858ddd33e3464e172a9e3a4bc2ec7e31acbb9c9c3ca6656af725ee466c3a82f2379b93d734bc53a49210c5422b74146e97b6bce5f1de3090e6'

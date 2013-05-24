69c6f6aeffa8f229af503e689468329392bcc324
Apprentice <apprentice@devbootcamp.com>
2013-05-24 15:28:53 -0700

    create multiple contact sending of texts

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,15 +3,19 @@ class MessagesController < ApplicationController
   def new
     @students = Student.all  #refactor to only show students for current_user
     @message = Message.new
-
   end
 
   def create
-    # @message = Message.create(params[:message])
-    # current_user.messages << @message
-    p params
-    # twilio_client.account.sms.messages.create(from: app_phone, to: '+17143811795', 
-    #   body: @message.content)
-     redirect_to root_path
+    contacts = Contact.where :id => params["recipients"].values
+    message = Message.create(params[:message])
+    current_user.messages << message
+    contacts.each do |contact|
+      contact.messages << message
+    end
+    contacts.each do |contact|
+      # twilio_client.account.sms.messages.create(from: app_phone, to: contact.phone_number, 
+      #   body: message.content)
+    end
+      redirect_to root_path
   end
 end
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,7 +1,6 @@
 class Message < ActiveRecord::Base
-  attr_accessible :content, :delivered, :header, :time_sent, :teacher_id, :contacts_attributes
+  attr_accessible :content, :delivered, :header, :time_sent, :teacher_id
   belongs_to :teacher
   has_and_belongs_to_many :contacts
 
-  accepts_nested_attributes_for :contacts
 end

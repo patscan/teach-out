cc3c08c284f8633911214d79f1889f16bc5159fb
Apprentice <apprentice@devbootcamp.com>
2013-05-24 12:27:58 -0700

    move sign up form to partial

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,4 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
-
 require 'faker'
-
 
 10.times do
   Teacher.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name,

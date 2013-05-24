87e96c2055d939df81b8fb50aa7297d642abbb9c
Apprentice <apprentice@devbootcamp.com>
2013-05-24 11:36:58 -0700

    made db seeds; added teacher_id to attr_accessible in Student class

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,4 +1,4 @@
 class Student < ActiveRecord::Base
-  attr_accessible :first_name, :last_name
+  attr_accessible :first_name, :last_name, :teacher_id
   belongs_to :teacher
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -15,3 +15,23 @@ require 'faker'
                  grade: "6th", zip: Faker::Address.zip)
 end
 
+n = 11
+10.times do
+  Student.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name,
+                 teacher_id: (n-=1))
+end
+
+
+Student.all.each do |student|
+ contact = Contacts.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, 
+                  email: Faker::Internet.email, relationship_to_student: "Guardian", 
+                  language: "English", phone_number: Faker::PhoneNumber.phone_number)
+  student.contacts << contact
+end
+
+Contact.all.each do |contact|
+  message = Message.create(header: "Mr. Brown, 6th Grade", 
+                           content: Faker::Lorem.sentence(5),
+                           delivered: true, time_sent: Time.now, teacher_id: (n-=1))
+  contact.messages << message
+end

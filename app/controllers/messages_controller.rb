c1e0be37552f97831907c77a2bcaf8e4c6d47f50
Apprentice <apprentice@devbootcamp.com>
2013-05-25 13:08:28 -0700

    updated messages controller to reflect db changes, added active field to true in seeds file, deleted un-used factories folder, added active to attr_accessible in contact model, updated new_message view to display header text in content box

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -6,24 +6,22 @@ class MessagesController < ApplicationController
   end
 
   def create
-
+    message = Message.create(params[:message].merge(
+      header: "Sent from: #{current_user.first_name} #{current_user.last_name}, #{current_user.school_name}"))
     students = Student.where :id => params[:students].values
+    current_user.messages << message
     contacts = []
     students.each do |student|
-      student.contacts.where("active=?", true)
-      
+      student.messages << message
+      student.contacts.where("active=?", true).each do |contact|
+        contact.messages << message
+        contacts << contact
+      end
     end
-
-    # message = Message.create(params[:message].merge(
-    #   header: "Sent from: #{current_user.first_name} #{current_user.last_name}, #{current_user.school_name}"))
-    # current_user.messages << message
-    # contacts.each do |contact|
-    #   contact.messages << message
-    # end
-    # contacts.each do |contact|
-    #   twilio_client.account.sms.messages.create(from: app_phone, to: contact.phone_number, 
-    #     body: message.content)
-    # end
+    contacts.each do |contact|
+      twilio_client.account.sms.messages.create(from: app_phone, to: contact.phone_number, 
+        body: message.content)
+    end
     redirect_to dashboard_teachers_path
   end
 
--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,5 +1,5 @@
 class Contact < ActiveRecord::Base
-  attr_accessible :email, :first_name, :language, :last_name, :phone_number, :relationship_to_student
+  attr_accessible :email, :first_name, :language, :last_name, :phone_number, :relationship_to_student, :active
   has_and_belongs_to_many :messages
   has_and_belongs_to_many :students
 end

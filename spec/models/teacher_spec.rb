ea2573781c202e60ff83a84eab7f8243ed697b70
patscan <patscan@gmail.com>
2013-05-25 14:30:34 -0700

    add teacher validation tests

--- spec/models/teacher_spec.rb
+++ spec/models/teacher_spec.rb
@@ -14,8 +14,11 @@ describe Teacher do
 
   context "with invalid attributes" do
     it { should validate_presence_of(:email) }
+    it { should validate_presence_of(:first_name) }
+    it { should validate_presence_of(:last_name) }
+    it { should validate_presence_of(:grade) }
+    it { should validate_presence_of(:zip) }
     it { should ensure_length_of(:password).is_at_least(5) }
-
   end
 
 end

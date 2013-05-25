bf49ad57657a00a90074d7c7fdf22e0fc786e342
Apprentice <apprentice@devbootcamp.com>
2013-05-24 19:42:33 -0700

    add password length validation to Teacher model

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,7 @@
 class HomeController < ApplicationController
   
   def index
+    @teacher = Teacher.new
   end
 
 end
--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -6,6 +6,7 @@ class Teacher < ActiveRecord::Base
 
   validates_presence_of   :email, :first_name, :grade, :last_name, :password, :zip
   validates_uniqueness_of :email
+  validates :password, :length => { :minimum => 5 }
 
   has_secure_password
 end

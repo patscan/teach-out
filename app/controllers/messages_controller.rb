58e02f478ce8df2b96b1e7d2ff4c7eef55e0f11f
Apprentice <apprentice@devbootcamp.com>
2013-05-25 12:01:49 -0700

    unfinished changes on messages controller for sending out messages

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -6,19 +6,24 @@ class MessagesController < ApplicationController
   end
 
   def create
-    contacts = Contact.where :id => params["recipients"].values
-    message = Message.create(params[:message].merge(
-      header: "Sent from: #{current_user.first_name} #{current_user.last_name}, #{current_user.school_name}"))
-    current_user.messages << message
-    contacts.each do |contact|
-      contact.messages << message
-    end
+
+    students = Student.where :id => params[:students].values
+    students.each do |student|
+      
+
+    # message = Message.create(params[:message].merge(
+    #   header: "Sent from: #{current_user.first_name} #{current_user.last_name}, #{current_user.school_name}"))
+    # current_user.messages << message
+    # contacts.each do |contact|
+    #   contact.messages << message
+    # end
     # contacts.each do |contact|
     #   twilio_client.account.sms.messages.create(from: app_phone, to: contact.phone_number, 
     #     body: message.content)
     # end
-      redirect_to root_path
   end
+  redirect_to dashboard_teachers_path
+end
 
 end
- 
+

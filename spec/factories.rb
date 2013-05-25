e513b98e3244b7262a2f06e88c6032f0989033cc
patscan <patscan@gmail.com>
2013-05-25 12:15:41 -0700

    first cut of factories for tests

---
+++ spec/factories.rb
@@ -0,0 +1,43 @@
+FactoryGirl.define do
+
+  factory :teacher do
+    first_name "Betty"
+    last_name "White"
+    email "betty@white.com"
+    password "password"
+    grade "5th"
+    zip "88924"
+    school_name "Lincoln"
+    title "Mrs."
+  end
+
+  factory :student do
+    teacher # including these objects to test relationships
+    contact # don't know if i need this
+    first_name "Bobby"
+    last_name "Wilson"
+  end  
+
+  factory :contact do
+    teacher
+    student
+    first_name "Jkai"
+    last_name "Hsu"
+    relationship_to_student "Parent"
+    language "English"
+    phone_number "+14157246575"
+    email "jkai@son.com"
+    active true
+  end
+
+  factory :message do
+    teacher
+    contact
+    student
+    header "Betty White, 6th Grade, Licoln"
+    content "Your son has been absent 3 days this week, please call 415-123-5678"
+    delivered false
+    subject "Absence"
+  end
+
+end

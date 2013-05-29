0cf560d244602ceb2194a5f73777fb48d3d8e96a
patscan <patscan@gmail.com>
2013-05-29 01:05:05 -0700

    dashboard redesign

--- app/assets/javascripts/slideshow.js
+++ app/assets/javascripts/slideshow.js
@@ -1,18 +1,11 @@
 function slideSwitch() {
   var $active = $('#slideshow span.active');
-
   if ( $active.length == 0 ) $active = $('#slideshow span:last');
-
-  var $next =  $active.next().length ? $active.next()
-  : $('#slideshow span:first');
-
+  var $next = $active.next().length ? $active.next() : $('#slideshow span:first');
   $active.addClass('last-active');
-
-  $next.css({opacity: 0.0})
-  .addClass('active')
-  .animate({opacity: 1.0}, 1000, function() {
-   $active.removeClass('active last-active');
-});
+  $next.css({opacity: 0.0}).addClass('active').animate({opacity: 1.0}, 1000, function() {
+    $active.removeClass('active last-active');
+  });
 }
 
 $(document).ready(function(){

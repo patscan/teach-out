af6c3f07a7458e8079dd7f6dab2eb3a43c8680bf
avalon9000 <kparkroy@gmail.com>
2013-05-30 19:17:30 -0700

    message time_sent

--- app/controllers/message_status_controller.rb
+++ app/controllers/message_status_controller.rb
@@ -1,10 +1,10 @@
 class StatusController < ApplicationController
 
   def create
-    message = ContactMessage.find_by_sms_sid(params[:SmsSid])
-    message.delivered = params[:SmsStatus] == "sent"
-    message.time_sent = Time.now
-    message.save!
+    contact_message = ContactMessage.find_by_sms_sid(params[:SmsSid])
+    contact_message.delivered = params[:SmsStatus] == "sent"
+    Message.find(contact_message.message_id).time_sent = Time.now
+    contact_message.save!
   end
 
 end

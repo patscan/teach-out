58424332ad2334c9bbf6173f94f4940677091e19
patscan <patscan@gmail.com>
2013-06-05 01:05:18 -0700

    Fix bug in calendar rendering
    It was breaking attempting to render next/previous months. Fixed by adding back .calendar class back into DOM.

--- app/controllers/teachers_controller.rb
+++ app/controllers/teachers_controller.rb
@@ -11,14 +11,14 @@
     if @teacher.save
       session[:id]=@teacher.id
       render :js => "window.location = '/teachers/dashboard'"
-    else      
+    else
       render :json => { :error => @teacher.errors.full_messages.join("<br/>") }, :status => 422
     end
   end
 
   def dashboard
     @teacher = current_user
-    @message = Message.new 
+    @message = Message.new
     @student = Student.new
     @student.contacts.build
     @students = current_user.students.order(:first_name)
@@ -27,21 +27,22 @@
     contact_messsages = ContactMessage.delivered.where(:message_id => ids)
     @sent_messages = messages
 
-    @messages_by_date = messages.group_by do |m| 
+    @messages_by_date = messages.group_by do |m|
       m.time_sent.getlocal.strftime("%Y-%m-%d") if m.time_sent
     end
     @date = params[:date] ? Date.parse(params[:date]) : Date.today
   end
 
-  def render_calendar 
+  def render_calendar
+    p params
     string_date = params[:month]
     year, month = string_date.split('-')
     @date = Date.new(year.to_i, month.to_i)
-    @messages_by_date = current_user.messages.group_by do |m| 
+    @messages_by_date = current_user.messages.group_by do |m|
       m.time_sent.strftime("%Y-%m-%d") if m.time_sent
     end
-    calendar = render_to_string(:partial => "shared/calendar", 
-                                :layout => false, 
+    calendar = render_to_string(:partial => "shared/calendar",
+                                :layout => false,
                                 :locals => {:date => @date, :messages_by_date => @messages_by_date})
     render :json => {:calendar => calendar}
   end
@@ -53,8 +54,8 @@
           m.time_sent.strftime("%Y-%m-%d") == @date
         end
       end
-    single_day = render_to_string(:partial => "shared/single_day", 
-                                  :layout => false, 
+    single_day = render_to_string(:partial => "shared/single_day",
+                                  :layout => false,
                                   :locals => {:date => @date, :day_messages => @day_messages })
     render :json => {:single_day => single_day}
   end
